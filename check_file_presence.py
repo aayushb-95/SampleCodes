c5fca3ab3cb2d1f65f40ea18991813ce17d1ff5d
bansal <aayush@luvis.ai>
2023-12-28 12:09:07 +0100

    updating codebase

--- check_file_presence.py
+++ check_file_presence.py
@@ -1,65 +1,69 @@
+# Python Internal Modules
 import os
+from typing import List
 
-# Example usage
-dir1 = <path_to_dir1>
-dir2 = <path_to_dir2>
-
-def get_filenames(directory):
-    filenames = []
-    for filename in os.listdir(directory):
-        if os.path.isfile(os.path.join(directory, filename)):
-            filenames.append(os.path.splitext(filename)[0])
-    return filenames
+SRC_DIR: str = "/src_files/"
+TRG_DIR: str = "/trg_files/"
+EXTENSION: str = "png"
 
 
-def check_files_presence(dir1, dir2):
-    dir1_filenames = get_filenames(dir1)
-    dir2_filenames = get_filenames(dir2)
+class CheckFilePresence:
+    """
+    CheckFilePresence class provides methods to compare and synchronize files between two directories.
+    """
 
-    not_present_in_dir1 = []
-    not_present_in_dir2 = []
+    @staticmethod
+    def __get_filenames(directory: str) -> List[str]:
+        return [os.path.splitext(filename)[0] for filename in os.listdir(directory) if
+                os.path.isfile(os.path.join(directory, filename))]
 
-    # Check filenames present in dir2 but not in dir1
-    for filename in dir2_filenames:
-        if filename not in dir1_filenames:
-            not_present_in_dir1.append(filename)
+    def __check_file_presence(self, src_dir: str, trg_dir: str) -> (List[str], List[str]):
+        src_dir_filenames = set(self.__get_filenames(src_dir))
+        trg_dir_filenames = set(self.__get_filenames(trg_dir))
 
-    # Check filenames present in dir1 but not in dir2
-    for filename in dir1_filenames:
-        if filename not in dir2_filenames:
-            not_present_in_dir2.append(filename)
+        files_not_present_in_src_dir = trg_dir_filenames - src_dir_filenames
+        files_not_present_in_trg_dir = src_dir_filenames - trg_dir_filenames
 
-    return not_present_in_dir1, not_present_in_dir2
+        return list(files_not_present_in_src_dir), list(files_not_present_in_trg_dir)
 
-not_present_in_dir1, not_present_in_dir2 = check_files_presence(dir1, dir2)
+    @staticmethod
+    def __display_missing_files(message: str, missing_files: List[str]):
+        if missing_files:
+            print(message)
+            for filename in missing_files:
+                print(filename)
+            print(f"Total count: {len(missing_files)}")
+        else:
+            print(f"All files from {message.split()[3]} are present in {message.split()[5]}")
+        print()  # Adding a new line for clarity
 
-if not_present_in_dir1:
-    print("Files present in dir2 but not in dir1:")
-    for filename in not_present_in_dir1:
-        print(filename)
-    print(f"Total count: {len(not_present_in_dir1)}")
-else:
-    print("All files from dir2 are present in dir1.")
+    @staticmethod
+    def remove_files(trg_dir: str, files_to_remove: List[str]):
+        if files_to_remove:
+            print(f"Removing files from {trg_dir}")
+            for filename in files_to_remove:
+                file_path = os.path.join(trg_dir, filename + EXTENSION)
+                os.remove(file_path)
+                print(f"Removed file from: {file_path}")
+            print("All files removed successfully")
+        else:
+            print(f"No files to remove from {trg_dir}")
 
-print()  # Adding a new line for clarity
+    def remove_mismatched_files(self, src_dir: str, trg_dir: str):
+        missing_files_src, missing_files_trg = self.__check_file_presence(src_dir, trg_dir)
 
-if not_present_in_dir2:
-    print("Files present in dir1 but not in dir2:")
-    for filename in not_present_in_dir2:
-        print(filename)
-    print(f"Total count: {len(not_present_in_dir2)}")
-else:
-    print("All files from dir1 are present in dir2.")
+        self.__display_missing_files(f"Files present in {trg_dir} but not in {src_dir}:", missing_files_src)
+        self.__display_missing_files(f"Files present in {src_dir} but not in {trg_dir}:", missing_files_trg)
 
-print()  # Adding a new line for clarity
+        # Remove files from dir2 that are not present in dir1
+        self.remove_files(trg_dir, missing_files_src)
 
-# Remove files from dir2 that are not present in dir1
-if not_present_in_dir1:
-    print("Removing files from dir2...")
-    for filename in not_present_in_dir1:
-        file_path = os.path.join(dir2, filename + ".png")
-        os.remove(file_path)
-        print(f"Removed: {file_path}")
-    print("Files removed successfully.")
-else:
-    print("No files to remove from dir2.")
+
+def main():
+    file_presence = CheckFilePresence()
+    file_presence.remove_mismatched_files(SRC_DIR, TRG_DIR)
+
+
+# ========= Main Entry Point =========
+if __name__ == "__main__":
+    main()
